1f92c8ae51f2a2b911e9385e8b6bfd82d43c58ce
JacobRajah <jacob.rajah@mail.utoronto.ca>
2022-03-23 13:39:26 -0400

    if/for functionality explored

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,33 +3,24 @@
     <!-- Use the Prop passed into the component -->
     <h1>{{ msg }}</h1> 
     <h2>{{ msg2 }}</h2>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    <h1>Our Tests</h1>
+    <!-- v-model will hold the data reference for storing input -->
+    <input v-model="my_input">
+    <p>Input value is: {{ my_input }}</p>
+    <p> Lets access a key : {{ sample.key1 }}</p>
+
+    <button @click="isVisible = !isVisible">Toggle</button>
+    <!-- Can run logical statements using v-if and v-else  -->
+    <div v-if="isVisible">Show content</div>
+    <div v-else>Hide Content </div>
+
+    <!-- Can run loops with v-for iterate through list data -->
+    <ul v-for="(post, index) in posts" :key="index">
+      <li>{{index}}. The name is {{post.name}} and the text is
+          {{post.text}}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
@@ -40,6 +31,15 @@ export default {
   props: {
     msg: String,
     msg2: String
+  },
+  data () {
+    return {
+      my_input : 'abc',
+      sample : { key1: 'cde' },
+      isVisible : true,
+      posts: [{name: 'Jane', text: 'my text 1'}, {name:
+            'John', text: 'my text 2'}]
+    }
   }
 }
 </script>
